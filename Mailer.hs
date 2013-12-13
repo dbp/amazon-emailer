22b1a986b4b269fd4ddd20465004631a366f96b0
Daniel Patterson <dbp@riseup.net>
2013-12-12 23:51:48 -0500

    changing queue processing to use database for locking, so many concurrent workers can be running at once (and move to git)

---
+++ Mailer.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Applicative
+import Control.Monad (forM, replicateM)
+import Control.Monad.Trans.Resource (runResourceT)
+import Control.Concurrent (threadDelay)
+import Control.Exception (bracket)
+import qualified Control.Exception.Lifted as EX
+import System.IO (hPutStr, stderr)
+import System.Directory (doesFileExist, removeFile)
+import Data.Text (Text)
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Encoding as LT
+import qualified Data.Text.Encoding as T
+import Data.Time.Clock (UTCTime, getCurrentTime)
+
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Network.HTTP.Conduit (Manager(..), newManager, closeManager, conduitManagerSettings)
+import Network.Mail.Mime (Mail(..), Address(..), Part(..), Encoding (QuotedPrintableText))
+import Network.Mail.Mime.SES
+
+import Config (configHost, configUser, configPass, 
+               configDB, configAccessKey,
+               configSecretKey, configLimit)
+
+main :: IO ()
+main =
+  bracket ((,) <$> (connect $ defaultConnectInfo { connectHost = configHost,
+                                                   connectUser = configUser,
+                                                   connectPassword = configPass,
+                                                   connectDatabase = configDB })
+               <*> (newManager conduitManagerSettings))
+    (\(c,m) -> close c >> closeManager m)
+    (\(c,m) -> runQueue c m)
+
+data AmEmail = AmEmail { aId :: Int, aTo :: Text, aToName :: Maybe Text, aFrom :: Text, 
+                         aFromName :: Text, aSubject :: Text, aBody :: Text, aHtml :: Bool } 
+             deriving (Show, Eq)
+
+instance FromRow AmEmail where
+  fromRow = AmEmail <$> field <*> field <*> field <*> field
+                    <*> field <*> field <*> field <*> field
+
+
+runQueue :: Connection -> Manager -> IO ()
+runQueue c m = do
+  now <- getCurrentTime
+  replicateM configLimit $ do
+    -- NOTE(dbp 2013-12-12): This query has all the magic in it: it grabs an email
+    -- off the queue, marks it as processing, and returns it. The nested query is to
+    -- do the limiting, and the seeming redundant 'and processing = false' is b/c
+    -- the inner query is in a separate transaction, so this could race (but the
+    -- result would just be to have no message found even if one existed in database.
+    email <- query_ c "update amazon_email_queue set processing = true where id = (select id from amazon_email_queue where sent_at is null and failed_count < 3 and processing = false order by date asc limit 1) and processing = false returning id, to_addr, to_name, from_addr, from_name, subject, body, html"
+    sent <- EX.catch
+            (sendEmails m email)
+            (\e -> do putStrLn $ show (e::EX.SomeException)
+                      execute c "update amazon_email_queue set failed_count = failed_count + 1, processing = false where id = ?" 
+                        (Only (aId $ head email))
+                      return [])
+    mapM (\i -> execute c "update amazon_email_queue set sent_at = ?, processing = false where id = ?" 
+                      (now, i))
+            sent
+    
+  threadDelay 1000000 -- note, this is a conservative processing of the queue, as we don't include
+                      -- the time that is spent actually sending the emails.
+  runQueue c m
+
+sendEmails :: Manager ->  [AmEmail] -> IO [Int]
+sendEmails m es =
+  forM es (\e -> do
+            runResourceT (renderSendMailSES m (mkSES e) (mkMail e))
+            return (aId e))
+
+mkSES :: AmEmail -> SES
+mkSES (AmEmail _ to _ from _ _ _ _) = SES (T.encodeUtf8 from) 
+                                        [T.encodeUtf8 to]
+                                        configAccessKey
+                                        configSecretKey
+
+mkMail :: AmEmail -> Mail
+mkMail (AmEmail _ to tname from fname subj body html) = 
+  Mail (Address (Just fname) from)
+       [(Address tname to)]
+       [] -- CC
+       [] -- BCC
+        [ ("Subject", subj) ]
+       [[Part (if html then "text/html; charset=utf-8" else "text/plain; charset=utf-8")
+         QuotedPrintableText Nothing []
+         (LT.encodeUtf8 $ LT.fromStrict body)]]
---
+++ README.md
@@ -0,0 +1,83 @@
+# About
+
+This is a really simple system for handling queued emails. It reads
+emails out of a database table, and tries to send a specified number
+every second (well, it delays a second between each sending, so it is
+upper bounded by the specified number per second, but probably will be
+less, accounting for latency, request time, etc). It records when a
+message was sent. Right now, only one process can operate (and it
+creates a lock file, /tmp/amazon_emailer.lock to prevent other
+processes from starting - see Todo for way to fix this).
+
+If messages fail to send for any reason (this could be connectivity,
+blacklisted addresses, etc, but NOT bounced messages, as those are
+sent, but come back later), the message has its failed_count
+incremented, and when that hits 3, we stop trying to send it. Messages
+are sent in plain text unless html is true, in which case they are
+sent as html. Right now there is no way to send both.
+
+# Software
+
+This is designed to work with Amazon's Simple Email Service (SES), and
+PostgreSQL. It wouldn't be too hard to adapt it to other backends, but
+I wanted to keep it extremely simple, and everything I do is with
+PostgreSQL.
+
+# Setup
+
+To use this, you need to create a table amazon_email_queue with the
+following colums:
+
+id serial PRIMARY KEY,
+to_addr text NOT NULL,
+to_name text,
+from_addr text NOT NULL,
+from_name text NOT NULL,
+subject text NOT NULL,
+body text NOT NULL,
+date timestamptz NOT NULL DEFAULT now(),
+sent_at timestamptz,
+failed_count integer NOT NULL DEFAULT 0,
+html boolean NOT NULL DEFAULT false
+processing boolean NOT NULL DEFAULT false
+
+And you should put an index on sent_at, date, failed_count, and processing (used to filter the messages).
+
+Then you should create a Config.hs file, that looks like:
+
+        {-# LANGUAGE OverloadedStrings #-}
+    
+    module Config where
+    
+    import Data.ByteString (ByteString)
+    
+    configHost :: String
+    configHost = "host for postgresql"
+    
+    configUser :: String
+    configUser = "user for postgresql"
+    
+    configPass :: String
+    configPass = "password for postgresql"
+    
+    configDB :: String
+    configDB = "database name"
+    
+    configAccessKey :: ByteString
+    configAccessKey = "AWS access key"
+    
+    configSecretKey :: ByteString
+    configSecretKey = "AWS secret key"
+    
+    configLimit :: Int
+    configLimit = 5 -- or higher, if you have a higher rate limit
+
+Finally, in your application, just insert messages into the
+amazon_email_queue table, and then should get picked up and sent out.
+
+
+# Todo
+
+It would be better to eliminate the file locking, and simply grab/mark grabbed the
+emails in a single database query, so that many emailers could be working on the database
+at once without the possibility of duplicate messages. This is an easy change.
